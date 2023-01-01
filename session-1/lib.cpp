a2088b1cb8a4e3b82592e274fa8e0bdeaebf1ac3
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2023-01-01 21:57:36 +0200

    Setup

--- session-1/lib.cpp
+++ session-1/lib.cpp
@@ -1,8 +1,47 @@
 #include "cinttypes"
-#include "cstdio"
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <string>
+
+namespace lib {
+class Example {
+public:
+  Example(const char *data) : data_(data) {}
+
+  const char *getCString() const { return data_.c_str(); }
+
+  std::size_t length() const { return data_.length(); }
+
+private:
+  std::string data_;
+};
+} // namespace lib
 
 extern "C" {
+uint32_t add_u32(uint32_t a, uint32_t b) { return a + b; }
+
 uint64_t double_to_bits(double value) {
-    return *reinterpret_cast<uint64_t*>(&value);
+  return *reinterpret_cast<uint64_t *>(&value);
+}
+
+double bits_to_double(uint64_t value) {
+  return *reinterpret_cast<double *>(&value);
+}
+
+lib::Example *lib__Example__create(const char *data) {
+  return new lib::Example(data);
+}
+
+void lib__Example__delete(lib::Example *example) { delete example; }
+
+void lib__Example__destructure(lib::Example *example) { example->~Example(); }
+
+const char *lib__Example__getCString(const lib::Example *example) {
+  return example->getCString();
+}
+
+std::size_t lib__Example__length(const lib::Example *example) {
+  return example->length();
 }
 }
--- session-1/mod.ts
+++ session-1/mod.ts
@@ -1,8 +1,51 @@
 const lib = Deno.dlopen(new URL("./libsession-1.so", import.meta.url), {
+  add_u32: {
+    parameters: ["u32", "u32"],
+    result: "u32",
+  },
   double_to_bits: {
     parameters: ["f64"],
     result: "i64",
   },
+  bits_to_double: {
+    parameters: ["i64"],
+    result: "f64",
+  },
+  lib__Example__create: {
+    parameters: ["buffer"],
+    result: "pointer",
+  },
+  lib__Example__delete: {
+    parameters: ["pointer"],
+    result: "void",
+  },
+  lib__Example__destructure: {
+    parameters: ["pointer"],
+    result: "void",
+  },
+  lib__Example__getCString: {
+    parameters: ["pointer"],
+    result: "pointer",
+  },
+  lib__Example__length: {
+    parameters: ["pointer"],
+    result: "usize",
+  },
 });
 
+console.log(lib.symbols.add_u32(2, 3));
 console.log(lib.symbols.double_to_bits(112346.243547357));
+console.log(
+  lib.symbols.bits_to_double(lib.symbols.double_to_bits(112346.243547357)),
+);
+const string = "My perfect string!!!\0";
+const stringBuffer = new TextEncoder().encode(string);
+const examplePointer = lib.symbols.lib__Example__create(stringBuffer);
+console.log(
+  Deno.UnsafePointerView.getCString(
+    lib.symbols.lib__Example__getCString(examplePointer),
+  ),
+);
+console.log(lib.symbols.lib__Example__length(examplePointer), string.length);
+lib.symbols.lib__Example__delete(examplePointer);
+console.log(lib.symbols.lib__Example__length(examplePointer), string.length);
